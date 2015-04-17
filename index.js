199e778c7a75fb12124972fc84b88e5595b425c7
TheConnMan <bcconn2112@gmail.com>
2015-04-17 15:26:38 +0000

    Formatting changes.

--- index.js
+++ index.js
@@ -14,34 +14,22 @@ var mailin = require('mailin');
  * Here disable the webhook posting so that you can do what you want with the
  * parsed message. */
 mailin.start({
-  port: 25,
-  disableWebhook: true // Disable the webhook posting.
-});
-
-/* Access simplesmtp server instance. */
-mailin.on('authorizeUser', function(connection, username, password, done) {
-  if (username == "johnsmith" && password == "mysecret") {
-    done(null, true);
-  } else {
-    done(new Error("Unauthorized!"), false);
-  }
+	port: 25,
+	disableWebhook: true // Disable the webhook posting.
 });
 
 /* Event emitted when a connection with the Mailin smtp server is initiated. */
 mailin.on('startMessage', function (connection) {
-  /* connection = {
-      from: 'sender@somedomain.com',
-      to: 'someaddress@yourdomain.com',
-      id: 't84h5ugf',
-      authentication: { username: null, authenticated: false, status: 'NORMAL' }
-  }
-  }; */
-  console.log(connection);
+		/* connection = {
+			from: 'sender@somedomain.com',
+			to: 'someaddress@yourdomain.com',
+			id: 't84h5ugf',
+			authentication: { username: null, authenticated: false, status: 'NORMAL' }
+		}
+	}; */
 });
 
 /* Event emitted after a message was received and parsed. */
 mailin.on('message', function (connection, data, content) {
-  console.log(data);
-  /* Do something useful with the parsed message here.
-   * Use parsed message `data` directly or use raw message `content`. */
+	console.log(data);
 });
